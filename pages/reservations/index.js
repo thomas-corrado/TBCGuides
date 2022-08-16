02de636ab5178a9303a3530e8edbc78fca08f99d
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-15 21:29:07 -0400

    start backend

--- pages/reservations/index.js
+++ pages/reservations/index.js
@@ -1,8 +1,42 @@
 import * as React from "react";
+import { useForm } from "react-hook-form";
 
 export default function ReservationsPage() {
+  const { register, handleSubmit } = useForm();
+
+  async function submitHandler(data) {
+    const response = await fetch("/api/form", {
+    		method: "POST",
+        	body: JSON.stringify(data),
+        	headers: {
+        		'Content-Type': 'application/json',
+      		},
+    	})
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <label htmlFor="first">First Name</label>
+        <input placeholder="John" {...register("first")} />
+
+        <label htmlFor="last">Last Name</label>
+        <input placeholder="Doe" {...register("last")} />
+
+        <label htmlFor="phone">Phone Numner</label>
+        <input placeholder="" {...register("phone")} />
+
+        <label htmlFor="email">Email</label>
+        <input placeholder="" {...register("email")} />
+
+        <label htmlFor="guests">Number of Guests</label>
+        <input placeholder="" {...register("guests")} />
+
+        <label htmlFor="date">Dates</label>
+        <input placeholder="" {...register("date")} />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
