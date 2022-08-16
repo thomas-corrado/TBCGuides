f4e520618fd79525c26621d6e388c958a2747c74
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-16 19:03:17 -0400

    add thumnail (hopefully)

--- components/aboutVideo.js
+++ components/aboutVideo.js
@@ -20,14 +20,23 @@ const AboutVideo = () => {
     return (
       <StyledOuterBox>
         <StyledInnerBox>
-          <div style={{ width: "100%", height: "auto", position: "relative", top: "50.3%", transform: "translateY(-50%)"}}>
+          <div
+            style={{
+              width: "100%",
+              height: "auto",
+              position: "relative",
+              top: "50.3%",
+              transform: "translateY(-50%)",
+            }}
+          >
             <video
               controls
               style={{
                 objectFit: "cover",
-                width: "100%", 
-                height: "100%"
+                width: "100%",
+                height: "100%",
               }}
+              preload="metadata"
             >
               <source
                 src="https://res.cloudinary.com/dsc1j5xny/video/upload/v1660687489/tbc_guide_services__michael_blatt__-_v3_720p_lyrudn.mp4"
