d779859e508385557f48fbe4aed874fa78e5661f
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-15 21:41:24 -0400

    try to work on deployment

--- pages/api/form.js
+++ pages/api/form.js
@@ -7,6 +7,7 @@ async function handler(req, res) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       },
       scopes: [
