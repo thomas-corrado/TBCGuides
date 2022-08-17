793f2b1f300645320b82b042ec6255f5368b0251
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-16 22:10:47 -0400

    add reservations button

--- components/aboutVideo.js
+++ components/aboutVideo.js
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Box } from "@mui/system";
 import { styled } from "@washingtonpost/wpds-ui-kit";
+import Button from "@mui/material/Button";
+import { useRouter } from "next/router";
 
 const StyledOuterBox = styled(Box, {
   width: "100vw",
   height: "calc(100vh - 4rem)",
   display: "flex",
   justifyContent: "center",
-  overflow: "hidden",
   position: "absolute",
   marginTop: "4rem"
 });
@@ -16,87 +17,108 @@ const StyledInnerBox = styled(Box, {
   position: "relative",
   height: "100%",
   width: "95vw",
-
 });
 
 const AboutVideo = () => {
+    const router = useRouter(); 
+
     return (
-      <StyledOuterBox className="outerbox">
-        <style global jsx>{`
-          html {
-            min-height: 100%;
-            height: 100%;
-          }
-
-          body {
-            height: 100%;
-            margin: 0;
-          }
-
-          .outerbox {
-            display: -webkit-box; /* OLD - iOS 6-, Safari 3.1-6 */
-            display: -moz-box; /* OLD - Firefox 19- (buggy but mostly works) */
-            display: -ms-flexbox; /* TWEENER - IE 10 */
-            display: -webkit-flex;
-            display: flex;
-            -webkit-box-align: center;
-            -webkit-flex-align: center;
-            -ms-flex-align: center;
-            -webkit-align-items: center;
-            align-items: center;
-            position: absolute;
-            height: calc(100% -4rem);
-            width: 100%;
-            top: 0;
-            z-index: 2;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-          }
-
-          .innerbox {
-            min-height: 10px;
-            min-width: 10px;
-            margin: auto;
-            position: relative;
-            display: table;
-          }
-
-          @media (max-width: 400px) {
-            .innerbox {
-              bottom: 4rem
+      <div>
+        <StyledOuterBox className="outerbox">
+          <style global jsx>{`
+            html {
+              min-height: 100%;
+              height: 100%;
             }
-          }
-        `}</style>
-        <StyledInnerBox className="innerbox">
-          <div
-            className="videoBox"
-            style={{
-              display: "table-cell",
-              verticalAlign: "middle",
-              overflow: "hidden",
-            }}
-          >
-            <video
-              controls
+
+            body {
+              height: 100%;
+              margin: 0;
+            }
+
+            .outerbox {
+              display: -webkit-box; /* OLD - iOS 6-, Safari 3.1-6 */
+              display: -moz-box; /* OLD - Firefox 19- (buggy but mostly works) */
+              display: -ms-flexbox; /* TWEENER - IE 10 */
+              display: -webkit-flex;
+              display: flex;
+              -webkit-box-align: center;
+              -webkit-flex-align: center;
+              -ms-flex-align: center;
+              -webkit-align-items: center;
+              align-items: center;
+              position: absolute;
+              height: calc(100vh - 4rem);
+              width: 100%;
+              top: 0;
+              z-index: 2;
+              -webkit-box-sizing: border-box;
+              -moz-box-sizing: border-box;
+              box-sizing: border-box;
+            }
+
+            .innerbox {
+              min-height: 10px;
+              min-width: 10px;
+              margin: auto;
+              position: relative;
+              display: table;
+            }
+
+            @media (max-width: 400px) {
+              .innerbox {
+                bottom: 2rem;
+              }
+            }
+          `}</style>
+          <StyledInnerBox className="innerbox">
+            <div
+              className="videoBox"
               style={{
-                objectFit: "cover",
-                width: "100%",
-                maxHeight: "calc(100vh - 4rem)",
+                display: "table-cell",
+                verticalAlign: "middle",
                 overflow: "hidden",
-                padding: 0,
-                margin: 0,
               }}
-              preload="metadata"
             >
-              <source
-                src="https://res.cloudinary.com/dsc1j5xny/video/upload/v1660687489/tbc_guide_services__michael_blatt__-_v3_720p_lyrudn.mp4#t=0.5"
-                type="video/mp4"
-              />
-            </video>
-          </div>
-        </StyledInnerBox>
-      </StyledOuterBox>
+              <video
+                controls
+                style={{
+                  objectFit: "cover",
+                  width: "100%",
+                  maxHeight: "calc(100vh - 12rem)",
+                  padding: 0,
+                  margin: 0,
+                }}
+                preload="metadata"
+              >
+                <source
+                  src="https://res.cloudinary.com/dsc1j5xny/video/upload/v1660687489/tbc_guide_services__michael_blatt__-_v3_720p_lyrudn.mp4#t=0.5"
+                  type="video/mp4"
+                />
+              </video>
+              <div
+                style={{
+                  zIndex: 3,
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  position: "relative",
+                  paddingBottom: "3rem",
+                  top: "2rem",
+                }}
+              >
+                <Button
+                  onClick={() => router.push(`/reservations`)}
+                  variant="contained"
+                  sx={{ fontFamily: "Raleway", backgroundColor: "black" }}
+                >
+                  Make a Reservation
+                </Button>
+              </div>
+            </div>
+          </StyledInnerBox>
+        </StyledOuterBox>
+      </div>
     );
 }
 
--- pages/about/index.js
+++ pages/about/index.js
@@ -3,11 +3,22 @@ import NavBar from "../../components/navBar";
 import AboutVideo from "../../components/aboutVideo";
 import { Box } from "@mui/system";
 import { styled } from "@washingtonpost/wpds-ui-kit";
+import Button from "@mui/material/Button";
 
+const StyledButtonBox = styled(Box, {
+    width: "100vw", 
+    display: "flex", 
+    justifyContent: "center", 
+    position: "relative", 
+    fontFamily: "Raleway", 
+    
+})
 
 export default function AboutPage() {
-  return <div>
-    <NavBar/>
-    <AboutVideo/>
-  </div>;
+  return (
+    <div>
+      <NavBar />
+      <AboutVideo />
+    </div>
+  );
 }
