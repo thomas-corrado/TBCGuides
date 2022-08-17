bcd44337d68eef1b27f1f7f8431f6409c66e3704
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-17 12:33:52 -0400

    try to fix nextconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-const withVideos = require('next-videos')
-
-module.exports = withVideos()
-
 module.exports = {
   images: {
-    domains: ["res.cloudinary.com", "d14drtvwlopsgs.cloudfront.net"]
+    domains: ["d14drtvwlopsgs.cloudfront.net"]
   },
 };
