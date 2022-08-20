7862d59208850c70caddc38c4749209357c32ec8
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-20 12:59:20 -0400

    change title name

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <div>
         <Head>
-          <title>Hanover Guides</title>
+          <title>TBC Guide Services</title>
           <meta
             name="viewport"
             content="initial-scale=1, viewport-fit=cover"
