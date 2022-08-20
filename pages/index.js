d9a73a36419eff7d05a75dd899593f7d9b877cef
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-08-20 10:30:33 -0400

    try fixing notch for iphone x3

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
       <div>
         <Head>
           <title>Hanover Guides</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/> 
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+        
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
