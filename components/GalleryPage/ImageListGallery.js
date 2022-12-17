4d82199ac99645b0d0223744046ae41412e50d24
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-12-17 17:49:58 -0500

    add more images to gallery

--- components/GalleryPage/ImageListGallery.js
+++ components/GalleryPage/ImageListGallery.js
@@ -43,8 +43,8 @@ const ImageListGallery = () => {
             >
               <ImageListItem>
                 <Image
-                  src={`https://s3.amazonaws.com/tbcguides.fish/winter-${item.number}.jpg`}
-                  alt={`https://s3.amazonaws.com/tbcguides.fish/winter-${item.number}.jpg`}
+                  src={`https://s3.amazonaws.com/tbcguides.fish/fish-holding-${item.number}.jpg`}
+                  alt={`https://s3.amazonaws.com/tbcguides.fish/fish-holding-${item.number}.jpg`}
                   className="complicated-image"
                   layout="fill"
                 />
@@ -134,5 +134,101 @@ const itemData = [
   },
   {
     number: 25,
+  },
+  {
+    number: 26,
+  },
+  {
+    number: 27,
+  },
+  {
+    number: 28,
+  },
+  {
+    number: 29,
+  },
+  {
+    number: 30,
+  },
+  {
+    number: 31,
+  },
+  {
+    number: 32,
+  },
+  {
+    number: 33,
+  },
+  {
+    number: 34,
+  },
+  {
+    number: 35,
+  },
+  {
+    number: 36,
+  },
+  {
+    number: 37,
+  },
+  {
+    number: 38,
+  },
+  {
+    number: 39,
+  },
+  {
+    number: 40,
+  },
+  {
+    number: 41,
+  },
+  {
+    number: 42,
+  },
+  {
+    number: 43,
+  },
+  {
+    number: 44,
+  },
+  {
+    number: 45,
+  },
+  {
+    number: 46,
+  },
+  {
+    number: 47,
+  },
+  {
+    number: 48,
+  },
+  {
+    number: 49,
+  },
+  {
+    number: 50,
+  },
+  {
+    number: 51,
+  },
+  {
+    number: 52,
+  },
+  {
+    number: 53,
+  },
+  {
+    number: 54,
+  },
+  {
+    number: 55,
+  },
+  {
+    number: 56,
+  },
+  {
+    number: 57,
   },
 ];
