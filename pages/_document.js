6b4e0a0e1f43e4a2b7a38169ac07017e700fd490
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2022-12-17 22:44:41 -0500

    add more labeling

--- pages/_document.js
+++ pages/_document.js
@@ -10,45 +10,47 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="English">
-        <Head>
-          <meta
-            name="google-site-verification"
-            content="EM19wPbtiFI0glqYg6zFRnKZLLSWg5hPKLGg4HmegVI"
-          />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Antonio:wght@700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=livory&display=optional"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=livory:wght@600&display=optional"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://use.typekit.net/nff0pei.css"
-          ></link>
-        </Head>
-        <body>
-          <div id="globalLoader">
-           <CircularProgress />
-          </div>
-          <Main />
-          <NextScript />
-        </body>
+        <html lang="English">
+          <Head>
+            <meta
+              name="google-site-verification"
+              content="EM19wPbtiFI0glqYg6zFRnKZLLSWg5hPKLGg4HmegVI"
+            />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin="true"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Antonio:wght@700&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=livory&display=optional"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=livory:wght@600&display=optional"
+              rel="stylesheet"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&display=swap"
+              rel="stylesheet"
+            />
+            <link
+              rel="stylesheet"
+              href="https://use.typekit.net/nff0pei.css"
+            ></link>
+          </Head>
+          <body>
+            <div id="globalLoader">
+              <CircularProgress />
+            </div>
+            <Main />
+            <NextScript />
+          </body>
+        </html>
       </Html>
     );
   }
