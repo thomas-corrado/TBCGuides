b7882341745a6e8ef19aebc9467a2e11981bd0a7
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2023-09-22 15:34:37 +0100

    add reservation disclaimer

--- components/ReservationsPage/ReservationsPageComponent.js
+++ components/ReservationsPage/ReservationsPageComponent.js
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import NavigationBarNonHome from "../General/NavigationBarNonHome";
 import ContactInfo from "../General/Contact/ContactInfo";
 import ReservationForm from "./ReservationForm";
@@ -6,9 +6,19 @@ import ReservationForm from "./ReservationForm";
 const ReservationsPageComponent = () => {
   return (
     <>
-      <Stack className="about-stack">
+      <Stack>
         <NavigationBarNonHome pages={pages} />
         <ReservationForm />
+        <Typography variant="h7_reservations" sx={{ width: "90vw", textAlign: 'center', display: 'flex', alignSelf: 'center'}} mb={5}>
+          All reservations require a 50% security deposit of the cost of the
+          trip. This security deposit becomes non-refundable 14 days preceding
+          the scheduled fishing date. Inclement weather days will be
+          rescheduled. The captain reserves the right to cancel any trip due to
+          weather conditions, or mechanical failures. If the captain cancels the
+          trip for any reason mentioned above, the customer&#39;s deposit shall
+          be refunded or credited to another reservation date if desired. All
+          deposits are forfeited after one year.
+        </Typography>
       </Stack>
       <ContactInfo />
     </>
--- pages/pontoon/index.js
+++ pages/pontoon/index.js
@@ -16,7 +16,7 @@ export default function PontoonPage() {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
         
-        <title>Pontoon</title>
+        <title>Pontoon Boat</title>
         <meta
           property="og:image"
           content="https://s3.amazonaws.com/tbcguides.fish/fishing-logo.jpg"
