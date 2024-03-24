137cfc47f20a60634b424ba16a062541a5ec0684
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2024-03-24 17:52:40 -0400

    clarify hours

--- components/General/Contact/GridItems/BusinessHours.js
+++ components/General/Contact/GridItems/BusinessHours.js
@@ -23,7 +23,7 @@ const BusinessHours = () => {
           Business Hours
         </Typography>
         <Typography variant="h8" className="contact-item" mb={1}>
-          Sunday-Saturday: 7:00 AM - 7:00 PM
+          Seven Days a Week, <br/> 7:00 AM - 7:00 PM
         </Typography>
 
         <Stack direction="row" spacing={2} mb={8}>
