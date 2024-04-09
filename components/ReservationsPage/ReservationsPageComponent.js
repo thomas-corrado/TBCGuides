d2fb2e5b148d590c35dce8817b6e1a60eb81df78
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2024-04-09 19:29:01 -0400

    change 'captain' to 'guide' in security deposit

--- components/ReservationsPage/ReservationsPageComponent.js
+++ components/ReservationsPage/ReservationsPageComponent.js
@@ -22,8 +22,8 @@ const ReservationsPageComponent = () => {
           All reservations require a 50% security deposit of the cost of the
           trip. This security deposit becomes non-refundable 14 days preceding
           the scheduled fishing date. Inclement weather days will be
-          rescheduled. The captain reserves the right to cancel any trip due to
-          weather conditions, or mechanical failures. If the captain cancels the
+          rescheduled. The guide reserves the right to cancel any trip due to
+          weather conditions, or mechanical failures. If the guide cancels the
           trip for any reason mentioned above, the customer&#39;s deposit shall
           be refunded or credited to another reservation date if desired. All
           deposits are forfeited after one year.
